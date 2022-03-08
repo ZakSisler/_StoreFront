1d4b4d4c361940d22ae2c498eb8f3d3b6e505b22
Zakary Sisler <zakarysisler@outlook.com>
2022-03-08 17:05:58 -0600

    I created Identity Samples, tested my admin creation/login and delete functionality

--- _StoreFront.UI.MVC/Startup.cs
+++ _StoreFront.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(_StoreFront.UI.MVC.Startup))]
 namespace _StoreFront.UI.MVC
 {
     public partial class Startup
