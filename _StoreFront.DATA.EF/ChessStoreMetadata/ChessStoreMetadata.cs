ae3e165aa0acb91830570b766ca00af4314b77d5
Zakary Sisler <zakarysisler@outlook.com>
2022-03-14 23:05:50 -0500

    I entered regions for my meta data and entered my get; set;'s -- next up: enter meta data and biz rules if applicable

--- _StoreFront.DATA.EF/ChessStoreMetadata/ChessStoreMetadata.cs
+++ _StoreFront.DATA.EF/ChessStoreMetadata/ChessStoreMetadata.cs
@@ -7,7 +7,53 @@ using System.ComponentModel.DataAnnotations; //added for metadata and validation
 
 namespace _StoreFront.DATA.EF//.ChessStoreMetadata - name must match the namespace of classes these partial classes will buddy up with.
 {
-    
+    #region Department Metadata
+    public class DepartmentMetadata
+    {
+        [Required]
+        [Display(Name = "Department Name")]
+        [StringLength(50, ErrorMessage = "*Max 50 characters")]
+        public string DepartmentName { get; set; }
+    }
+
+    [MetadataType(typeof(DepartmentMetadata))]
+    public partial class Department { }
+
+    #endregion
+
+    #region Employee Metadata
+
+    public class EmployeeMetadata
+    {
+        public string FirstName { get; set; }
+
+
+        public string LastName { get; set; }
+
+
+        public string PositionTitle { get; set; }
+
+
+        public Nullable<int> DirectReportID { get; set; }
+
+
+        public int DepartmentID { get; set; }
+    }
+
+    #endregion
+
+    #region Product Metadata
+
+    #endregion
+
+    #region Product Category Metadata
+
+    #endregion
+
+    #region Product Status Metadata
+
+    #endregion
+
 
 
 }
