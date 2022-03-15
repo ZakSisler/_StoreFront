07ade1f54184969c6586846242abd92b16ede851
Zakary Sisler <zakarysisler@outlook.com>
2022-03-14 22:12:05 -0500

    I changed some meta data

---
+++ _StoreFront.DATA.EF/ChessStoreMetadata/ChessStoreMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; //added for metadata and validation
+
+namespace _StoreFront.DATA.EF//.ChessStoreMetadata - name must match the namespace of classes these partial classes will buddy up with.
+{
+    
+
+
+}
