4ef130d5790770bae7de29a38875648eb5125995
Zakary Sisler <zakarysisler@outlook.com>
2022-03-17 22:09:49 -0500

    I updated names, made links to different pages in layout

--- _StoreFront.UI.MVC/Controllers/HomeController.cs
+++ _StoreFront.UI.MVC/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace _StoreFront.UI.MVC.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
